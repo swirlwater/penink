3287b31ab58bae4c601253246f2b2b6bf1717874
penink <whx1978938887@student.gdufe.edu.cn>
2024-04-14 22:40:46 +0800

    项目初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: 'Penink',
+    description: 'Just playing around',
+    locales: {
+        '/': {
+            lang: 'en-US',
+            title: 'VuePress',
+            description: 'Vue-powered Static Site Generator'
+        },
+        '/zh/': {
+            lang: 'zh-CN',
+            title: 'VuePress',
+            description: 'Vue 驱动的静态网站生成器'
+        }
+    }
+}
